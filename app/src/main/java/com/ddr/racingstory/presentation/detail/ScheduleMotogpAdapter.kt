dd08f4d6b5d35295f3a95417ad33ae15b16a665c
dbesar <dededarirahmadi@gmail.com>
2022-12-18 00:51:46 +0700

    add schedule motogp adapter and add more item schedule layout

---
+++ app/src/main/java/com/ddr/racingstory/presentation/detail/ScheduleMotogpAdapter.kt
@@ -0,0 +1,51 @@
+package com.ddr.racingstory.presentation.detail
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ddr.core_model.Competition
+import com.ddr.core_model.Schedule
+import com.ddr.racingstory.R
+import com.ddr.racingstory.databinding.ItemCompetitionBinding
+import com.ddr.racingstory.databinding.ItemScheduleMotogpBinding
+import com.ddr.racingstory.presentation.main.CompetitionAdapter
+
+/**
+ * Created by Dede Dari Rahmadi on 18/12/22
+ */
+
+class ScheduleMotogpAdapter : RecyclerView.Adapter<ScheduleMotogpAdapter.ViewHolder>() {
+
+    private val listData = mutableListOf<Schedule>()
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun setData(data: List<Schedule>?) {
+        listData.clear()
+        listData.addAll(data ?: emptyList())
+        notifyDataSetChanged()
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder = ViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.item_schedule_motogp, parent, false)
+    )
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val currentData = listData[position]
+        holder.bind(currentData)
+    }
+
+    override fun getItemCount() = listData.size
+
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val binding = ItemScheduleMotogpBinding.bind(itemView)
+        fun bind(data: Schedule) {
+            binding.apply {
+                schedule = data
+            }
+        }
+    }
+}
---
+++ core-model/src/main/java/com/ddr/core_model/Schedule.kt
@@ -0,0 +1,13 @@
+package com.ddr.core_model
+
+import java.util.Date
+
+/**
+ * Created by Dede Dari Rahmadi on 18/12/22
+ */
+data class Schedule(
+    val id: String,
+    val date: String,
+    val nation: String,
+    val nameRace: String
+)
